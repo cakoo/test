b71ceea4305504a9492397ace9477ba680686097
weiyong <wayyong@126.com>
2017-02-12 10:39:26 +0800

    node.js简单小程序

--- web/js/server.js
+++ web/js/server.js
@@ -8,7 +8,6 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // 发送响应数据 "Hello World"
-    response.end('author: weiyong\n');
     response.end('Hello World\n');
 }).listen(8888);
 
